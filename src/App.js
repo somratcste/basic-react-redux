9379ca74f72ec0af82d8dacc0587d31cca279855
somratcste <somratcste@gmail.com>
2021-05-08 12:32:44 +0200

    feat: use reduxjs toolkit to manage store

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Auth from './components/Auth';
+import Counter from './components/Counter';
+import Header from './components/Header';
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Auth />
+            <Counter />
+        </>
+    );
+}
+
+export default App;
